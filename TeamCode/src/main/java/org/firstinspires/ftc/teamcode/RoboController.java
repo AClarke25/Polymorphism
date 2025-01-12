8e4e265a53f2866c4717e8c12e95c64329c87ff2
Anna <115884637+AClarke25@users.noreply.github.com>
2025-01-12 12:02:51 -0500

    redoing hanging arm and auto modes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboController.java
@@ -19,7 +19,7 @@ public class RoboController {
     public DcMotor BLW;
     public DcMotor BRW;
 
-    // arm parts=
+    // arm parts
     public DcMotor VLS; // linear slide for outtake (Vertical Linear Slide)
     public DcMotor HLS; // linear slide for intake (Horizontal Linear Slide)
     public Servo outClaw; // bucket
@@ -27,6 +27,7 @@ public class RoboController {
     public Servo wrist; // upper part
     public Servo inClaw; //servo closest to white inclaw
     public Servo specimenArm;
+    public DcMotor hangingArm;
 
     // button logic variables
     public boolean inClawLastState;
@@ -61,6 +62,9 @@ public class RoboController {
 
         // specimen arm
         specimenArm = hardwareMap.get(Servo.class,"specimenArm");
+
+        // hanging arm
+        hangingArm = hardwareMap.get(DcMotor.class,"hangingArm");
 
         // logic booleans
         inClawLastState = false;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SoloDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SoloDrive.java
@@ -203,5 +203,15 @@ public class SoloDrive extends LinearOpMode {
         }
 
         roboController.specimenArmLastState = gamepad.dpad_up;
+
+        // solo driver will use right and left dpad (only in 2-person drive) to control
+        // hanging arm linear slide
+        if(gamepad.dpad_right){
+            roboController.hangingArm.setPower(1);
+        } else if(gamepad.dpad_left) {
+            roboController.hangingArm.setPower(-1);
+        } else {
+            roboController.hangingArm.setPower(0);
+        }
     }
 }
